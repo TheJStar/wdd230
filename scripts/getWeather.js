6791561c639d5289f13bf81392e6af201095cbd8
TheeJStar <kingoflightj@gmail.com>
2024-05-22 09:56:40 -0700

    hid my open weather api link and key

--- scripts/getWeather.js
+++ scripts/getWeather.js
@@ -1,7 +1,3 @@
-// 40.19218660447296, 44.51122431620101 : of Yerevan
-// https://api.openweathermap.org/data/2.5/weather?lat={40.19}&lon={44.51}&appid={4a974b3f0729cb02d19957576cc5d21c}
-// activate your api key
-
 https://openweathermap.org/img/wn/10d@2x.png
 
 const curretTemp = document.querySelector("#current-temp");
