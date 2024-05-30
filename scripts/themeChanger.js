05679b1740f2cb540c7d48360b512d58309d6cb2
TheeJStar <kingoflightj@gmail.com>
2024-05-30 09:26:49 -0700

    fixed some errors from the validators and fixed some links

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -10,6 +10,9 @@ const h2 = document.querySelectorAll("h2");
 const person = document.querySelector(".person-name");
 const cardListItems = document.querySelectorAll(".week");
 
+const html = document.querySelector("html");
+let light = true;
+
 themeButton.addEventListener('click', () => {
     themeButton.classList.toggle("dark");
     header.classList.toggle("dark");
@@ -34,6 +37,19 @@ themeButton.addEventListener('click', () => {
     h2.forEach(h => {
         h.classList.toggle("dark");
     })
+    if (light) {
+        light = false
+        html.style.setProperty("--base-color", "#2d5d4d");
+        html.style.setProperty("--background-color", "#141414");
+        html.style.setProperty("--text-color", "white");
+        html.style.setProperty("--accent-color", "aqua");
+    } else {
+        light = true
+        html.style.setProperty("--base-color", "aquamarine");
+        html.style.setProperty("--background-color", "white");
+        html.style.setProperty("--text-color", "black");
+        html.style.setProperty("--accent-color", "blue");
+    }
 });
 
 // this is used to changedthe prefered colors making it shorter 
@@ -41,7 +57,7 @@ themeButton.addEventListener('click', () => {
 
 // const html = document.querySelector("html");
 // let light = true;
-//
+
 // themeButton.addEventListener('click', () => {
 //     if (light) {
 //         light = false
